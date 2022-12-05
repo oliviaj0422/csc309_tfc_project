0d29981a5de9208a2adedc3b5e6f954c18187468
zzqjames <90412404+zzqjames@users.noreply.github.com>
2022-12-05 01:52:05 -0500

    Delete backend directory

--- backend/classes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from classes.models import Class,ClassInstance,UserEnrolledClass
-admin.site.register(Class)
-admin.site.register(ClassInstance)
-admin.site.register(UserEnrolledClass)
