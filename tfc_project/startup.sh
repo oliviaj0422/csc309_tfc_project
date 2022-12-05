57fa7defa459426240d9408a5402b161e2b768ec
zzqjames <90412404+zzqjames@users.noreply.github.com>
2022-12-05 00:58:19 -0500

    delete

--- tfc_project/startup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m pip install virtualenv
-virtualenv -p `which python3.10` venv
-source venv/bin/activate
-pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
