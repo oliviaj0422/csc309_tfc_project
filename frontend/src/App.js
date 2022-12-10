0500ea3f9d966d5ccc98d5b12b84fc1f2ce34202
oliviaj0422 <79121950+oliviaj0422@users.noreply.github.com>
2022-12-09 21:12:48 -0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import NavBar from './components/NavBarElem';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { useState, useEffect, createContext } from 'react';
-import { LoginContext } from './Contexts/LoginContext';
+import "./App.css";
+import NavBar from "./components/NavBarElem";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect, createContext } from "react";
+import { LoginContext } from "./Contexts/LoginContext";
 import Login from './AccountPages/Login';
 import Memberships from './AccountPages/Memberships';
 import Register from './AccountPages/Register';
@@ -11,6 +11,11 @@ import PaymentHistory from './AccountPages/PaymentHistory';
 import PersonalProfile from './AccountPages/ProfilePage/Profile';
 import EditProfile from './AccountPages/EditProfile';
 import UpdateCard from './AccountPages/EditCard';
+// import SearchClasses from "./components/SearchClasses/SearchClasses";
+// import MyClassHistory from "./components/MyClassHistory/MyClassHistory";
+// import GetClasses from "./components/GetClasses/GetClasses";
+// import MySchedule from "./components/MySchedule/MySchedule";
+// import Studios from "./components/Studios/studios";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(
@@ -30,6 +35,12 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/memberships" element={<Memberships />} />
+          {/* <Route path="/classes" element={<GetClasses />} />
+          <Route path="/my_schedule" element={<MySchedule />} />
+          <Route path="/my_class_history" element={<MyClassHistory />} />
+          <Route path="/search_classes" element={<SearchClasses />} />
+          <Route path="/my_studios" element={<Studios />} />
+          <Route path="/" element={<Studios />} exact/> */}
           <Route path="/register" element={<Register />} />
           <Route path="/add-card" element={<AddCard />} />
           <Route path="/payment-history" element={<PaymentHistory />} />
